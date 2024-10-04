a904e350b3bda1d9e5818fa0dc5e5d9faf5a4ea6
aamerhussain134 <106589207+aamerhussain134@users.noreply.github.com>
2024-10-04 12:20:03 +0500

    user page

---
+++ src/data/UserData.js
@@ -0,0 +1,28 @@
+import logo from "../assets/images/dpc.png";
+export const Useritem = [
+    {
+        "name": "Abdullah",
+        "email": "abdullah@gmail.com",
+        "homeTown": "Town",
+        "dob": "18/2/2000",
+        "schoolName": "Name here",
+        "graduationYear": "2023",
+        "sports": "Baseball",
+        "position": "Position",
+        "interests": "Health & Care, Fitness",
+        "logo": logo
+      },
+      {
+        "name": "John Doe",
+        "email": "john@example.com",
+        "homeTown": "New York",
+        "dob": "15/6/1998",
+        "schoolName": "High School Name",
+        "graduationYear": "2021",
+        "sports": "Football",
+        "position": "Striker",
+        "interests": "Fitness, Sports",
+        "logo":logo
+      }
+     
+];
--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -1,7 +1,59 @@
-import React from 'react'
+import React from "react";
+import { FaEllipsisV } from "react-icons/fa";
+import {Useritem} from "../data/UserData"; // Import the JSON file
 
 export const Users = () => {
   return (
-    <div>Users</div>
-  )
-}
+    <>
+      <div className="bg-white p-6 rounded-lg shadow-lg">
+        <h2 className="text-2xl font-semibold text-gray-800 mb-4">Users</h2>
+        <div className="overflow-y-auto overflow-x-auto w-full">
+          <table className="min-w-full bg-white rounded-lg shadow-lg">
+            <thead>
+              <tr className="text-left text-sm font-semibold text-[#828282] border-b border-gray-200">
+                <th className="p-4">Company logo</th>
+                <th className="p-4">Home Town</th>
+                <th className="p-4">Date of Birth</th>
+                <th className="p-4">School Name</th>
+                <th className="p-4">
+                  School Graduation <br /> Year
+                </th>
+                <th className="p-4">Sports</th>
+                <th className="p-4">Sports Position</th>
+                <th className="p-4">Interests</th>
+                <th className="p-4 text-center">Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              {Useritem.map((user, index) => (
+                <tr key={index} className="text-sm text-black border-b border-gray-200">
+                  <td className="p-2">
+                    <div className="flex items-center">
+                      <img src={user.logo} alt="logo" className="w-full rounded-full" />
+                      <div className="flex flex-col min-w-[150px] ml-4">
+                        <p className="text-sm font-medium">{user.name}</p>
+                        <p className="text-[12px] text-black whitespace-normal">{user.email}</p>
+                      </div>
+                    </div>
+                  </td>
+                  <td className="p-4">{user.homeTown}</td>
+                  <td className="p-4">{user.dob}</td>
+                  <td className="p-4">{user.schoolName}</td>
+                  <td className="p-4">{user.graduationYear}</td>
+                  <td className="p-4">{user.sports}</td>
+                  <td className="p-4">{user.position}</td>
+                  <td className="p-4">{user.interests}</td>
+                  <td className="px-4 py-2 text-right">
+                    <button className="text-gray-600 focus:outline-none">
+                      <FaEllipsisV />
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </>
+  );
+};
